8d69726e16f66d30023b9e0158b3b95dcc65a462
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-06-26 12:29:32 -0400

    drill 1 complete, buttons change p text

---
+++ src/state-drills/helloWorld.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class HelloWorld extends React.Component{
+
+state = {
+    who: null   
+};
+
+handleClick = whoItIs => {
+    console.log(whoItIs);
+   
+    this.setState({
+       
+            ...this.state.obj,
+            who: whoItIs
+
+          
+    });
+    console.log(this.state)
+    
+};
+render(){
+
+    return (
+    <div className ="button-display">
+        <p>Hello {this.state.who}</p>
+        <button onClick={() => this.handleClick('Friend')}>Friend</button>
+        <button onClick={() => this.handleClick('React')}>React</button>
+        <button onClick={() => this.handleClick('World')}>World</button>
+    </div>
+    )
+    
+}
+}
+

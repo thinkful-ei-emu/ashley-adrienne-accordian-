aee4447564d7884da2feafa30aecd2027cf173b5
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-06-26 17:40:12 -0400

    completed accordian drill

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import Accordian from './state-drills/accordian';
 
-class App extends Component {
-  static defaultProps ={
-    sections: [{title, content}]
-  }
+// array of objects each with a name and content
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
-
+class App extends React.Component {
   render() {
-    
-
-    return (
-      <div className="App">
-        <Accordian />
-        
-      </div>
-    );
+    return <div>
+      <Accordian sections={sections} />
+    </div>
   }
 }
 
 export default App;
+
+
+
+
--- src/state-drills/accordian.js
+++ src/state-drills/accordian.js
@@ -1,39 +1,108 @@
 import React from 'react';
+import App from '../App';
 
-export default class Accordian extends React.Component {
-//props named sections
-    state = {
-        isOpen: false
-    }
+//do not need to use app.js file
 
-    handleClick = () => {       
-        this.setState({
-            isOpen: !this.state.isOpen
-        },() => {console.log(this.state)})
-    };
+// export default class Accordian extends React.Component {
+// //props named sections
+// static defaultProps ={
+//     sections: []
+//   }
+    
+
+// state = {
+//         isOpen: false
+//     }
+
+//     handleClick = () => {       
+//         this.setState({
+//             isOpen: !this.state.isOpen
+//         },() => {console.log(this.state)})
+//     };
+
+//     renderContent() {
+//         const currentSection = this.props.sections[this.state.currentSectionIndex]
+//         return (
+//           <div className='content'>
+//             {currentSection.content}
+//           </div>
+//         )
+//       }
+     
+//       renderButtons() {
+//         return this.props.sections.map((section, index) => (
+//           <button key={index} onClick={this.handleButtonClick}>
+//             {section.title}
+//           </button>
+//         ))
+//       }
 
 
-render(props) {
+// render() {
+//     const buttons = this.props.sections.map((section, index) => (
+//         <button key={index}>
+//           {section.title}          
+//         </button>
+//       ))
 
-        return (
-            <div>
-                <ul>
-                    <li>
-                        <button onClick={() => this.handleClick()}></button>
-                        <p></p>
-                     </li>
-                     <li>
-                        <button></button>
-                        <p></p>
-                     </li>
-                     <li>
-                        <button></button>
-                        <p></p>
-                     </li>              
 
-                </ul>
-            </div>
 
-        )
-    }
+//         return (
+//             <div>
+//                 <ul className="accordian-tabs">
+//                     <li>
+//                     <div>
+//                         {this.renderButtons()}
+//                         {!!this.props.sections.length && this.renderContent()}
+//                     </div>
+//                      </li>
+                    
+                               
+
+//                 </ul>
+//             </div>
+
+//         )
+//     }
+// }
+
+
+export default class Accordion extends Component {
+  static defaultProps = {
+    sections: []
+  };
+
+  state = {
+    activeSectionIndex: null,
+  }
+
+  handleSetActiveSecion = (sectionIndex) => {
+    this.setState({ activeSectionIndex: sectionIndex })
+  }
+
+  renderItem(section, idx, activeSectionIndex) {
+    return (
+      <li className='Accordion__item' key={idx}>
+        <button
+          type='button'
+          onClick={() => this.handleSetActiveSecion(idx)}
+        >
+          {section.title}
+        </button>
+        {(activeSectionIndex === idx) && <p>{section.content}</p>}
+      </li>
+    )
+  }
+
+  render() {
+    const { activeSectionIndex } = this.state
+    const { sections } = this.props
+    return (
+      <ul className='Accordion'>
+        {sections.map((section, idx) =>
+          this.renderItem(section, idx, activeSectionIndex)
+        )}
+      </ul>
+    )
+  }
 }
